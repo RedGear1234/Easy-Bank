6865e96f58b35198b3a406f242d69cee20856a22
RedGear1234 <89768051+RedGear1234@users.noreply.github.com>
2022-02-13 21:50:33 +0530

    Bank

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+
+// let closesvg2 = document.getElementById("closesvg2")
+
+// closesvg2.addEventListener("click",function () {
+//     let remove = document.getElementById("hovercontainer")
+//     remove.style.display = "none"
+// })
+
+// let closesvg3 = document.getElementById("closesvg3")
+
+// closesvg3.addEventListener("click",function () {
+//     let remove = document.getElementById("hovercontainer")
+//     remove.style.display = "none"
+// })
+
+// let closesvg4 = document.getElementById("closesvg4")
+
+// closesvg4.addEventListener("click",function () {
+//     let remove = document.getElementById("hovercontainer")
+//     remove.style.display = "none"
+// })
